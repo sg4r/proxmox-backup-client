bb17810c38972d17249b67425ac17dd079638cbf
root <root@pbs.client.ct8.novalocal>
2021-03-27 14:22:18 +0000

    update build

--- pbs.build.sh
+++ pbs.build.sh
@@ -1,14 +1,6 @@
-git clone git://git.proxmox.com/git/proxmox
-git clone git://git.proxmox.com/git/proxmox-backup.git
+git clone --branch v1.0.11 git://git.proxmox.com/git/proxmox-backup.git
 
 cd proxmox
-git checkout 4dec479d2c46bd8cec28c1faa17aa013307de764
 patch -p1 <../proxmox.patch
 
-cd ../proxmox-backup
-git checkout 2d87f2fb73b9629abdfac18aefac213b9130a609
-patch -p1 <../pbs.patch
-cargo vendor
-
-cargo build --release --bin pxar
-cargo build --release --bin proxmox-backup-client
+cargo build --release --bin proxmox-backup-client --bin pxar --bin dump-catalog-shell-cli
