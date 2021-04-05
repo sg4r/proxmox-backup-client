0228c7094511d4896ab7efff099e56ee0377c53f
sg4r <41240574+sg4r@users.noreply.github.com>
2021-04-05 19:14:31 +0200

    Update pbs.build.sh

--- pbs.build.sh
+++ pbs.build.sh
@@ -1,6 +1,11 @@
 git clone --branch v1.0.11 git://git.proxmox.com/git/proxmox-backup.git
+git clone git://git.proxmox.com/git/proxmox
 
-cd proxmox-backup
+cd proxmox
+git checkout 1fce0ff41ddeb177f92874bf4e95a775cfd99c69
+patch -p1 <../proxmox.patch
+
+cd ../proxmox-backup
 patch --forward --strip=1 --input=../pbs.patch
 patch --forward --strip=1 --input=../fix-map_err.patch
 
